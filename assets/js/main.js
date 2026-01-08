c9ca3cec16c4a56f0085e04b7f8f07f8152848af
enginyanteri <enginyanteri@gmail.com>
2026-01-08 23:00:34 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const submenu = document.querySelector('.submenu');
 
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+  navLinks.classList.toggle('show');
 });
 
+dropdownToggle.addEventListener('click', () => {
+  submenu.classList.toggle('show');
+});

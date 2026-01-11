5774ee2cd4df3252a3754cc03fa858ac5ccf6aed
enginyanteri <enginyanteri@gmail.com>
2026-01-11 16:32:55 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,3 @@ const navLinks = document.getElementById("navbar").querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-// Header scroll efekti
-const header = document.querySelector('.top-bar');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 10) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
